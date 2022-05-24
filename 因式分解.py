1e4d15da02fd11b3881bac69f0364576ebbc2777
zhj1234567890 <105108489+zhj1234567890@users.noreply.github.com>
2022-05-24 08:38:16 +0800

    Add files via upload

---
+++ 因式分解.py
@@ -0,0 +1,3 @@
+from sympy import * # You need to install the SymPy symbol calculation library in advance
+x, y, z = symbols('x y z')
+print( factor(x**3 - x**2 + x - 1) )#type Primitive formula here
