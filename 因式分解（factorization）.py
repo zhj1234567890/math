eae72ed3a2327fc2153b5f5aaa29ad8abb3e2b6c
zhj1234567890 <105108489+zhj1234567890@users.noreply.github.com>
2022-07-08 13:30:17 +0800

    Update 因式分解（factorization）.py

--- 因式分解（factorization）.py
+++ 因式分解（factorization）.py
@@ -1,3 +1,3 @@
-from sympy import * # You need to install the SymPy symbol calculation library in advance
+from sympy import * #需要sympy函数库
 x, y, z = symbols('x y z')
-print( factor(x**3 - x**2 + x - 1) )#type Primitive formula here
+print( factor(x**3 - x**2 + x - 1) )#键入原式，不能省略乘号
