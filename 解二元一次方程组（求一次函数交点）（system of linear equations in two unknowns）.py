006374fffc4c1b85520102f18b4143e2b20d958b
zhj1234567890 <105108489+zhj1234567890@users.noreply.github.com>
2022-07-08 13:32:09 +0800

    Update 解二元一次方程组（求一次函数交点）（system of linear equations in two unknowns）.py

--- 解二元一次方程组（求一次函数交点）（system of linear equations in two unknowns）.py
+++ 解二元一次方程组（求一次函数交点）（system of linear equations in two unknowns）.py
@@ -1,4 +1,4 @@
-from sympy import #You need to install the SymPy symbol calculation library in advance
+from sympy import #需要sympy函数库
 x=Symbol(x)
 y=Symbol(y)
 print(solve([2*x+5*y-12,4*x-y-2],[x,y]))#键入解析式或方程组,不能省略乘号
