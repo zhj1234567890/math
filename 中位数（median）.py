edcf470692d60e81250677b00556b4a38798e13c
zhj1234567890 <105108489+zhj1234567890@users.noreply.github.com>
2022-07-08 13:28:54 +0800

    Update 中位数（median）.py

--- 中位数（median）.py
+++ 中位数（median）.py
@@ -1,9 +1,9 @@
-test = [1,5,6,4,1,4,5,1,12,2,20,10,10]#type your sample data here
+test = [1,5,6,4,1,4,5,1,12,2,20,10,10]#在此键入你的数据
 print(sorted(test))
-#even numbers
+#偶数
 if len(test)%2 == 0:
     mid = (sorted(test)[int(len(test)/2)] + sorted(test)[int(len(test)/2)+1])/2
-#odd number
+#奇数
 else:
     mid = sorted(test)[int(len(test)/2)]
 print(mid)
