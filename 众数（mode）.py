ad3e52953aa179f0e4b3f9535e026050641e7b0c
zhj1234567890 <105108489+zhj1234567890@users.noreply.github.com>
2022-07-08 13:29:22 +0800

    Update 众数（mode）.py

--- 众数（mode）.py
+++ 众数（mode）.py
@@ -1,6 +1,6 @@
 import numpy as np
 import statistics
-my_list = [1, 1, 2, 3, 4, 1, 2, 3, 3, 3, 3, 3]#Type your sample data here
+my_list = [1, 1, 2, 3, 4, 1, 2, 3, 3, 3, 3, 3]#在此键入你的数据
 mode1 = statistics.mode(my_list)
 mode2 = np.argmax(np.bincount(my_list))
 print(mode1)
